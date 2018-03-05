778412e0f3cc9c514bee3f738dd12767d7d9aaaa
Gergely Kalapos <gergo@kalapos.net>
2018-03-05 17:30:34 +0100

    Adding .NET Concept of the Week 9 Sample code
    Sample code for this video: https://www.youtube.com/watch?v=5HSKDGHijdI

--- samples/MonoSanityClient/Examples.cs
+++ samples/MonoSanityClient/Examples.cs
@@ -7,51 +7,64 @@ using WebAssembly;
 
 namespace MonoSanityClient
 {
-    public static class Examples
-    {
-        public static string AddNumbers(int a, int b)
-            => (a + b).ToString();
+	public static class Examples
+	{
 
-        public static string RepeatString(string str, int count)
-        {
-            var result = new StringBuilder();
+		public static string GetWinnerDotNet()
+		{
+			var winner = MeetupAttendeeListFacebook
+				.Concat(MeetupAttendeeListMeetupDotCom)
+				.Distinct()
+				.Except(AlreadyWon)
+				.ElementAt((new Random()).Next(MeetupAttendeeListFacebook.Count + MeetupAttendeeListFacebook.Count - 1 - AlreadyWon.Count));
 
-            for (var i = 0; i < count; i++)
-            {
-                result.Append(str);
-            }
+			AlreadyWon.Add(winner);
+			return winner;
+		}
 
-            return result.ToString();
-        }
+		public static List<String> AlreadyWon = new List<String>();
 
-        public static void TriggerException(string message)
-        {
-            throw new InvalidOperationException(message);
-        }
+		public static List<string> MeetupAttendeeListFacebook = new List<string>
+		{
+			"FbUser1",
+			"FbUser2",
+			"FbUser3"
+		};
 
-        public static string EvaluateJavaScript(string expression)
-        {
-            // For tests that call this method, we'll exercise the 'InvokeJSArray' code path
-            var result = Runtime.InvokeJSArray<string>(out var exceptionMessage, "evaluateJsExpression", expression, null, null);
-            if (exceptionMessage != null)
-            {
-                return $".NET got exception: {exceptionMessage}";
-            }
+		public static List<string> MeetupAttendeeListMeetupDotCom = new List<string>
+		{
+			"MeetupUser1",
+			"MeetupUser2",
+			"MeetupUser3"
+		};
+		public static void TriggerException(string message)
+		{
+			throw new InvalidOperationException(message);
+		}
 
-            return $".NET received: {(result ?? "(NULL)")}";
-        }
+		public static string EvaluateJavaScript(string expression)
+		{
+			// For tests that call this method, we'll exercise the 'InvokeJSArray' code path
+			var result = Runtime.InvokeJSArray<string>(out var exceptionMessage, "evaluateJsExpression", expression, null, null);
+			if (exceptionMessage != null)
+			{
+				return $".NET got exception: {exceptionMessage}";
+			}
 
-        public static string CallJsNoBoxing(int numberA, int numberB)
-        {
-            // For tests that call this method, we'll exercise the 'InvokeJS' code path
-            // since that doesn't box the params
-            var result = Runtime.InvokeJS<int, int, object, int>(out var exceptionMessage, "divideNumbersUnmarshalled", numberA, numberB, null);
-            if (exceptionMessage != null)
-            {
-                return $".NET got exception: {exceptionMessage}";
-            }
+			return $".NET received: {(result ?? "(NULL)")}";
+		}
 
-            return $".NET received: {result}";
-        }
-    }
+		public static string CallJsNoBoxing(int numberA, int numberB)
+		{
+			// For tests that call this method, we'll exercise the 'InvokeJS' code path
+			// since that doesn't box the params
+			var result = Runtime.InvokeJS<int, int, object, int>(out var exceptionMessage, "divideNumbersUnmarshalled", numberA, numberB, null);
+			if (exceptionMessage != null)
+			{
+				return $".NET got exception: {exceptionMessage}";
+			}
+
+			return $".NET received: {result}";
+		}
+	}
 }
